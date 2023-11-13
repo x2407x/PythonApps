683371ed86bb1753b0163abf9dc8b0da0ce68ff7
x2407x <150726220+x2407x@users.noreply.github.com>
2023-11-13 18:01:55 -0500

    Delete WebScrappingTest.py

--- WebScrappingTest.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-r = requests.get("http://en.wikipedia.org/wiki/List_of_countries_by_population ")
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
-for link in soup.find_all('a'):
-    print(link.get('href'))
